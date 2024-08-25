799981d6ed30422434cc6b086f12fbc328bbdd82
Lucas Alves <sociallucasalves@gmail.com>
2024-08-25 16:18:06 -0300

    feat: primeiros passos projeto django

---
+++ pypro/base/views.py
@@ -0,0 +1,8 @@
+# from django.shortcuts import render
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return HttpResponse("Ola django hello django ola mundo hello world sem amldição aqui usei todos possiveis")
